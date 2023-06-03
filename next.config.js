6037b4c2fe8623cbf3ab0cbc969c1dd2b4dd4834
vicarb <108682674+vicarb@users.noreply.github.com>
2023-06-03 11:44:24 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //comment this part when deploying in vms
   experimental: {
     appDir: true,
   },
   output: 'export',
+  //
   images: {
     domains: ['storage.googleapis.com'],
   },
