92471cfb24d06dc9e06f98c10f0f6c7bca9dc3bd
vicarb <vcardenasb@protonmail.com>
2023-06-05 16:19:27 -0400

    updaded success page gp

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Success from '@/components/Success/Success';
+import Navbar from '@/components/Navbar/Navbar';
 const SuccessPage = ({params}) => {
   console.log(params);
   
   return (
-    <div><Success/></div>
+    <>
+    <Navbar/>
+    <Success/>
+    </>
   )
 }
 export default SuccessPage;
