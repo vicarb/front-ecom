50db717bd3f4c4966752ffc000bec6c6af41f847
vicarb <108682674+vicarb@users.noreply.github.com>
2023-06-06 22:59:52 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //comment this part when deploying in vms
-  // experimental: {
-  //   appDir: true,
-  // },
-  // output: 'export',
+  experimental: {
+  appDir: true,
+  },
+  output: 'export',
   //
   // basePath:'/home',
   images: {
